67eafba4580c13e066f8b386b38952a0dd5f53ad
Dustin Kane <dustin@blisspointmedia.com>
2019-07-26 17:51:54 -0700

    index file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as stickyLib from "./StickyTable";
+
+export * from "./useScrollbarSizes";
+export * from "./useVirtual";
+export * from "./useSortHeader";
+
+export const basicRender = stickyLib.basicRenderer;
+export const StickyTable = stickyLib.StickyTable;
+export default StickyTable;
